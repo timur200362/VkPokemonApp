af84b087453784dbd2b6a4d1d19f97f174df884f
timur200362 <kh-g-2016@mail.ru>
2024-05-10 23:00:29 +0300

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,83 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.kotlin.serialization)
+    id("org.jetbrains.kotlin.kapt")
+}
+
+android {
+    namespace = "com.example.vkpokemonapp"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
+        multiDexEnabled = true
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.composeKotlinCompiler.get()
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.core.ktx)
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+
+    implementation(libs.activity.compose)
+    implementation(platform(libs.compose.bom))
+    implementation(libs.compose)
+    implementation(libs.compose.runtime)
+    implementation(libs.compose.graphics)
+    implementation(libs.compose.material.icons)
+    implementation(libs.compose.tooling.preview)
+    implementation(libs.compose.tooling)
+    implementation(libs.compose.kotlin.compiler)
+    implementation(libs.compose.material.icons.core)
+    implementation(libs.material)
+    implementation(libs.material3)
+    implementation(libs.androidx.compose.material)
+    implementation(libs.material3.android)
+    implementation(libs.lifecycle.compose.compiler)
+
+    implementation(libs.navigation.compose)
+
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.logging)
+    implementation(libs.koin)
+    implementation(libs.koin.compose)
+    implementation(libs.koin.test)
+
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.kotlin.serialization)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.retrofit.json.converter)
+
+    implementation(libs.coil)
+    implementation(libs.coil.svg)
+    implementation(libs.coil.compose)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(platform(libs.compose.bom))
+    androidTestImplementation(libs.ui.test.junit4)
+}
---
+++ app/src/main/java/com/example/vkpokemonapp/presentation/screens/PokemonsListScreen.kt
@@ -0,0 +1,9 @@
+package com.example.vkpokemonapp.presentation.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.material3.Text
+
+@Composable
+fun PokemonsListScreen() {
+    Text(text = "Screen")
+}
